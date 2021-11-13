fab335a7684e0fb90b1d5d6ef0a1a12d63ea8b28
Live Blogger <61611286+livebloggerofficial@users.noreply.github.com>
2021-11-13 11:58:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const sidebarContainer = document.querySelector(".sidebar-container");
+const detailsBtn = document.querySelector(".sidebar-container .details-btn");
+
+detailsBtn.addEventListener("click", () => {
+  sidebarContainer.classList.toggle("active");
+});
